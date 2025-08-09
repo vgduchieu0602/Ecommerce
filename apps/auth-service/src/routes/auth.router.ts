dbad0de13936e39547c786ddfc7a072bc0838ad6
vgdhieu0602 <vg.dhieu0602@gmail.com>
2025-08-09 19:04:54 +0700

    09/08 update auth route

--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -1,11 +1,20 @@
 import express, { Router } from "express";
-import { loginUser, userRegistration, verifyUser } from "../controller/auth.controller";
+import {
+  loginUser,
+  userForgotPassword,
+  userRegistration,
+  userResetPassword,
+  verifyUser,
+  verifyUserForgotPassword,
+} from "../controller/auth.controller";
 
 const router: Router = express.Router();
 
 router.post("/user-registration", userRegistration);
 router.post("/verify-user", verifyUser);
 router.post("/login-user", loginUser);
-
+router.post("/forgot-password-user", userForgotPassword);
+router.post("/reset-password-user", userResetPassword);
+router.post("/verify-forgot-password-user", verifyUserForgotPassword);
 
 export default router;
